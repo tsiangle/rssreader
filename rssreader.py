fa113522c77b40f7c64b1041226f8d62e04ccbc4
tsiangle <1120392462@qq.com>
2016-06-09 10:54:54 +0800

    very easy demo

--- rssreader.py
+++ rssreader.py
@@ -1,6 +1,18 @@
 
-
+import xmltodict
 import urllib.request
 
 class rssreader:
+	def __init__(self):
+		pass
 
+	def getpage(url):
+		return urllib.request.urlopen(url).read().decode()
+
+	def xml2dict(xml):
+		return xmltodict.parse(xml)
+
+	def show(jstring):
+		print(jstring)
+
+rssreader.show(rssreader.xml2dict(rssreader.getpage("http://www.zhihu.com/rss")))
